faff40d5670f92c86aa4911af7ef0247b7e4f496
Cheng Huang <huangchengadam@gmail.com>
2022-02-28 16:11:03 +0100

    readme

--- src/component/NavHeader/index.jsx
+++ src/component/NavHeader/index.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 import { NavBar, Toast} from 'antd-mobile'
+import { withRouter } from 'react-router-dom';
 import './index.scss'
 
-export default class NavHeader extends Component {
-    render() {
-        const {children} = this.props
+function NavHeader({history,children}) {
+
         return (
             <div>
                 <NavBar 
                     icon={<i className="iconfont icon-back"/>}
                     className='navbar'
-                    onClick = {()=>{this.props.history.go(-1)}}
+                    onClick = {()=>{history.go(-1)}}
                     >{children}</NavBar>
             </div>
         )
-    }
+    
 }
+
+//withRouter(NavHeader) 的返回值仍然是一个组件
+export default withRouter(NavHeader)
